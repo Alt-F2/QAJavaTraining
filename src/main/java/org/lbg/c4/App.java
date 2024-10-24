cf937c18701629abae4169441f03f9504a4d9622
and <and@and.com>
2024-10-24 09:41:47 +0100

    removed classA + classB, added new function to App class.

--- src/main/java/org/lbg/c4/App.java
+++ src/main/java/org/lbg/c4/App.java
@@ -1,18 +1,31 @@
 package org.lbg.c4;
 
-import org.lbg.unreachable.ClassB;
+import java.io.IOException;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-
-        int x = 33;
-
-        System.out.println("Hello World! " + x);
-
-        ClassA ca = new ClassA();
-        ClassB cb = new ClassB();
+    public static void main(String[] args) throws IOException {
+        int counter = 0;
+        int b;
+        try {
+            while ((b = System.in.read()) != -1) {
+                System.out.println(b);
+                counter++;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("No. of chars in ASCII: " + counter);
+        try {
+            while ((b = System.in.read()) != -1) {
+                System.out.println((char)b);
+                counter++;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println("No. of chars in alpha: " + counter);
     }
 }
